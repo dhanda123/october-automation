9ebd1497d66fe2b1963ead3abe805ac9965f633f
Sudhanshu <Sudhanshu@Shweta-PC>
2017-10-27 20:27:19 +0530

    october git

---
+++ Testing world/src/abcdef/C.java
@@ -0,0 +1,28 @@
+package abcdef;
+import org.testng.annotations.Test;
+import org.testng.annotations.*;
+
+public class C {
+@Test(groups={"P1"})
+public void pattern( )
+{
+	for(int i=1;i<=5;i++)
+		{
+		for(int k=1;k<i;k++)
+		{
+			System.out.print(" ");
+		}
+		for(int j=5;j>=i;j--)
+		{
+			System.out.print("*"+ " ");
+		}
+	System.out.println();
+	}
+}
+@Test
+public void pattern2()
+{
+	System.out.println("hello");
+}
+
+}
